34793713965e75710698315961cfdef9f57caa0e
deeksha9450 <deekshabajpai02@gmail.com>
2023-08-24 22:48:26 +0530

    set up view engine

---
+++ controllers/post_controller.js
@@ -0,0 +1,3 @@
+module.exports.postcontroller=function(req,res){
+    return res.send('This is post controller');
+}
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
- router.get('/',homeController.home);
+
+router.get('/',homeController.home);
+//for any further routes, access from here
+// router.use('/routername',require('./router_file_name'));
+router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
+
+
 module.exports = router;
